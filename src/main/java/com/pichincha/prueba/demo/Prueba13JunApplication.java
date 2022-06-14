b6cc44d8097ae271f3a0185c7828295b05e1e99f
MauricioCollantes <g87collantes@gmail.com>
2022-06-13 19:13:22 -0500

    Carga inicial proyecto

---
+++ src/main/java/com/pichincha/prueba/demo/Prueba13JunApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.prueba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba13JunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba13JunApplication.class, args);
+	}
+
+}
