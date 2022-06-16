644932168692f72facbf210d46278f66c56330f9
MauricioCollantes <g87collantes@gmail.com>
2022-06-16 17:25:49 -0500

    Carga proyecto 2

---
+++ src/main/java/com/pichincha/prueba/demo/dto/UserStoreDto.java
@@ -0,0 +1,21 @@
+package com.pichincha.prueba.demo.dto;
+
+import java.sql.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserStoreDto {
+	private Long id;
+	private String userName;
+	private Date createdDate;
+	
+	private List<ProductOrderDto> requestOrders;
+}
